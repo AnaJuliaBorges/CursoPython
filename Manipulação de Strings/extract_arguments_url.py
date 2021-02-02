a3d38a5251984269efb6d1960c82ef1767463897
najub <najuborgess@hotmail.com>
2021-02-02 16:41:09 -0300

    Métodos de string: Upper, Lower, Replace

--- Manipulação de Strings/extract_arguments_url.py
+++ Manipulação de Strings/extract_arguments_url.py
@@ -1,7 +1,7 @@
 class ExtractArgumentsURL:
     def __init__(self, url):
         if self.str_is_valid(url):
-            self.url = url
+            self.url = url.lower()
         else:
             raise LookupError("URL inválida !!")
 
@@ -20,11 +20,28 @@ class ExtractArgumentsURL:
         end_substring_origin = self.url.find("&")
         origin_currency = self.url[begin_substring_origin: end_substring_origin]
 
-        begin_substring_destiny = self.find_index_begin_substring(destiny_currency_search)
+        if (origin_currency == 'moedadestino'):
+            origin_currency = self.verify_currency_origin(origin_currency_search)
 
-        destiny_currency = self.url[begin_substring_destiny:]
+        begin_substring_destiny = self.find_index_begin_substring(destiny_currency_search)
+        end_substring_destiny = self.url.find("&valor")
+
+        destiny_currency = self.url[begin_substring_destiny:end_substring_destiny]
 
         return origin_currency, destiny_currency
 
     def find_index_begin_substring(self, value):
         return self.url.find(value) + len(value) + 1
+
+    def verify_currency_origin(self, search_origin_currency):
+        self.url = self.url.replace("moedadestino", "real", 1)
+        begin_substring_origin = self.find_index_begin_substring(search_origin_currency)
+
+        end_substring_origin = self.url.find("&")
+        return self.url[begin_substring_origin:end_substring_origin]
+
+    def return_value(self):
+        search_value = "valor"
+        begin_substring_value = self.find_index_begin_substring(search_value)
+        value = self.url[begin_substring_value:]
+        return value
--- Manipulação de Strings/main.py
+++ Manipulação de Strings/main.py
@@ -1,5 +1,6 @@
 from extract_arguments_url import ExtractArgumentsURL
 
+'''
 url = "https://bytebank.com/cambio?moedaorigem=real&moedadestino=dolar"
 
 argumentsURL = ExtractArgumentsURL(url)
@@ -7,3 +8,10 @@ origin_currency, destiny_currency = argumentsURL.returns_currency()
 
 print(origin_currency, destiny_currency)
 
+'''
+
+url = "https://www.bytebank.com.br/cambio?moedaorigem=real&moedadestino=dolar&valor=1500"
+exchange = ExtractArgumentsURL(url)
+origin_currency, destiny_currency = exchange.returns_currency()
+value = exchange.return_value()
+print(origin_currency, destiny_currency, value)
