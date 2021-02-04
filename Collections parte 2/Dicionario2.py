f02ddd3aab55614fd453fe3d8848628f90d3f2d4
najub <najuborgess@hotmail.com>
2021-02-04 13:34:37 -0300

    mais dicionario2

--- Collections parte 2/Dicionario2.py
+++ Collections parte 2/Dicionario2.py
@@ -1,12 +1,21 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 my_text = "Olá meu nome é ana Júlia e quase me chamei maria júlia " \
           "gosto de gosto doce e meu doce favorito é doce de coco".lower()
 
-word_appered = defaultdict(int)
-for word in my_text.split():
-    until_now = word_appered[word]
-    word_appered[word] = until_now + 1
+word_appered = Counter(my_text.split())
 
 for word, value in word_appered.items():
-    print(word, value)
+    print(word,"apareceu", value, "x")
+
+print("\n")
+
+class Account:
+    def __init__(self):
+        print("Criando Conta...")
+
+accounts = defaultdict(Account)
+accounts[26]
+accounts[34]
+accounts[15]
+accounts[34]
