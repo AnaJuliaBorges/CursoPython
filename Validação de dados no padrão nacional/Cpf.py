e7dcb750881325a5fddd4e272024139ca3b4ac41
najub <najuborgess@hotmail.com>
2021-03-02 14:54:07 -0300

    att validando cpf

--- Validação de dados no padrão nacional/Cpf.py
+++ Validação de dados no padrão nacional/Cpf.py
@@ -3,20 +3,19 @@ from validate_docbr import CPF
 class Cpf:
     def __init__(self, document):
         document = str(document)
-        if self.cpf_eh_Valido(document):
+        if self.cpf_is_valid(document):
             self.cpf = document
         else:
             raise ValueError("CPF inválido!")
 
-    def cpf_eh_Valido(self, cpf):
+    def cpf_is_valid(self, cpf):
         if len(cpf) == 11:
             return CPF().validate(cpf)
         else:
             raise ValueError("Quantidade de dígitos inválida!")
 
     def format_cpf(self):
-        mascara = CPF()
-        return mascara.mask(self.cpf)
+        return CPF().mask(self.cpf)
 
     def __str__(self):
         return self.format_cpf()
