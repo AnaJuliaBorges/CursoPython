0e985b6d333489a781c1317370eb96c1c1d1196c
najub <najuborgess@hotmail.com>
2021-03-02 16:20:03 -0300

    Validando telefones

--- Validação de dados no padrão nacional/main.py
+++ Validação de dados no padrão nacional/main.py
@@ -1,4 +1,5 @@
 from ValidDocument import Document
+from ValidPhone import ValidPhone
 
 example_cpf = "15316264754"
 cpf = Document.create_document(example_cpf)
@@ -7,3 +8,7 @@ print(cpf)
 example_cnpj = "35379838000112"
 cnpj = Document.create_document(example_cnpj)
 print(cnpj)
+
+telephone = "552126481234"
+telephone_object = ValidPhone(telephone)
+print(telephone_object)
