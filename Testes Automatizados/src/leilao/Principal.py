f8ac1307c94c6df08a32f4d4ba3c2e9a7d68359d
najub <najuborgess@hotmail.com>
2021-03-03 15:07:28 -0300

    Remodelando as classes de domino

---
+++ Testes Automatizados/src/leilao/Principal.py
@@ -0,0 +1,19 @@
+from dominio import Usuario, Lance, Leilao, Avaliador
+
+gui = Usuario('Gui')
+yuri = Usuario('Yuri')
+
+lance_do_yuri = Lance(yuri, 100.0)
+lance_do_gui = Lance(gui, 150.0)
+
+leilao = Leilao('Celular')
+
+leilao.lances.append(lance_do_gui)
+leilao.lances.append(lance_do_yuri)
+
+for lance in leilao.lances:
+    print(f'O usuario {lance.usuario.nome} deu um lance de {lance.valor}')
+
+Avaliador().avalia(leilao)
+
+print(f'O menor lance foi de {Avaliador().menor_lance} e o maior lance foi de {Avaliador().maior_lance}')
