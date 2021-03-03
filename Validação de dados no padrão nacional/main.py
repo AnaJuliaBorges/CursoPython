49933565cfa7a287136096a638a79d598bf0495c
najub <najuborgess@hotmail.com>
2021-03-03 10:16:49 -0300

    Manipulando datas

--- Validação de dados no padrão nacional/main.py
+++ Validação de dados no padrão nacional/main.py
@@ -1,5 +1,9 @@
 from ValidDocument import Document
 from ValidPhone import ValidPhone
+from datetime import datetime, timedelta
+
+from DatasBr import DatasBr
+
 
 example_cpf = "15316264754"
 cpf = Document.create_document(example_cpf)
@@ -12,3 +16,17 @@ print(cnpj)
 telephone = "552126481234"
 telephone_object = ValidPhone(telephone)
 print(telephone_object)
+
+date_today = DatasBr()
+print(date_today)
+
+date_today.registration_moment_difference()
+
+
+
+# hoje = datetime.today()
+# amanha = datetime.today() + timedelta(days=1)
+# print(hoje)
+# print(amanha)
+#
+# print(hoje - amanha)
